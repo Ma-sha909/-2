56a3f33c5074fb65b4155afcddc20b9a4b514e64
Ma-sha909 <mariart.96.com@gmail.com>
2024-10-10 19:38:33 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,10 @@
 #'Нули ничто, отрицание недопустимо'
 my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
 a = 0
-while 1 > 0 :
-    if int(my_list[int(a)]) >= 0 :
+while a < len(my_list) :
+    if int(my_list [int(a)]) >= 0 :
         print(my_list[int(a)])
-        if int(a) < len(my_list):
-            a = int(int(a) + 1)
-        else:
-            break
+        a = int(int(a) + 1)
+        continue
     else:
         break
