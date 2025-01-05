bfc4ceb988fd81574ed16f88f9fb574a6e3b75a2
Ma-sha909 <mariart.96.com@gmail.com>
2025-01-05 22:13:06 +0300

    Add files via upload

---
+++ module_8_3.py
@@ -0,0 +1,61 @@
+#Домашнее задание по теме "Создание исключений"
+#Задача "Некорректность"
+class Car:
+    def __init__(self, model, vin, numbers):
+        self.model = str(model)
+        if self.__is_valid_vin(vin) == True:
+            self.vin = vin
+        if self.__is_valid_numbers(numbers) == True:
+            self.numbers = numbers
+
+    def __is_valid_vin(self, vin):
+        if isinstance(vin, int) is True:
+            if vin >= 1000000 and vin <= 9999999:
+                return True
+            else:
+                raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        else:
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+
+    def __is_valid_numbers(self, numbers):
+        if isinstance(numbers, str) is True:
+            if len(numbers) == 6:
+                return True
+            else:
+                raise IncorrectCarNumbers('Неверная длина номера')
+        else:
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+try:
+    first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{first.model} успешно создан')
+try:
+    second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{second.model} успешно создан')
+try:
+    third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{third.model} успешно создан')
